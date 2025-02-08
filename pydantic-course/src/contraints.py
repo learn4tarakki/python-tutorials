84209302be5273cc3f82134d435bbb89b7e0f0bf
anup.gupta <learn4tarakki@gmail.com>
2025-02-08 16:52:25 +0530

    add field example

--- pydantic-course/src/contraints.py
+++ pydantic-course/src/contraints.py
@@ -2,15 +2,17 @@ from typing_extensions import Annotated
 from pydantic import BaseModel, Field
 from devtools import debug 
 
+# use of Annotated is best practice 
 
 class User(BaseModel):
-    id: Annotated[int, Field(gt=0)]  # best practice, instead of using Field directly
+    id: Annotated[int, Field(gt=0)]  
     name: Annotated[str, Field(max_length=10)]
-
+    role: str = Field(max_length=10, description="role")
 
 user = User(
     id=1,
-    name="ms dhoni",
+    name="raj",
+    role="designer"
 )
 
 debug(user)
